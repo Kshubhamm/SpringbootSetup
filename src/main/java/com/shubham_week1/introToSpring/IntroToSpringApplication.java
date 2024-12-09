48f231dc4f30d5ba6510cd27294ecfef90283964
kshubhammm0411 <shubham.kumar@timelyai.com>
2024-12-10 00:53:45 +0530

    Setting up

---
+++ src/main/java/com/shubham_week1/introToSpring/IntroToSpringApplication.java
@@ -0,0 +1,13 @@
+package com.shubham_week1.introToSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSpringApplication.class, args);
+	}
+
+}
